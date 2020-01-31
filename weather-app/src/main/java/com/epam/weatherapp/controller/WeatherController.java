12694b1ef65ddf983d34e45906eb5b655dbf661d
Prerna Gandhi <Prerna_Gandhi@epam.com>
2020-01-31 16:00:14 +0530

    Added configuration for swagger 2

--- weather-app/src/main/java/com/epam/weatherapp/controller/WeatherController.java
+++ weather-app/src/main/java/com/epam/weatherapp/controller/WeatherController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.epam.weatherapp.dto.SearchCriteria;
 import com.epam.weatherapp.dto.WeatherDetailsDTO;
 import com.epam.weatherapp.exceptionhandling.DefaultExceptionHandler;
@@ -23,16 +22,23 @@ import com.epam.weatherapp.exceptionhandling.LocationAlreadyPresentException;
 import com.epam.weatherapp.mapper.WeatherDetailsMapper;
 import com.epam.weatherapp.service.WeatherService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 @RestController
+@Api(value = "Weather App")
 public class WeatherController {
 	@Autowired
 	WeatherService weatherService;
 	@Autowired
 	WeatherDetailsMapper weatherDetailsMapper;
 
+	@ApiOperation(value = "Get weather details for a particular city or state or country or any combinations")
 	@PostMapping("/weather-details/search")
 	public ResponseEntity<List<WeatherDetailsDTO>> getWeatherDetails(
-			@RequestBody(required = false) SearchCriteria searchCriteria) throws DefaultExceptionHandler {
+			@ApiParam(value = "Provide search criteria object to seach weather details") @RequestBody(required = false) SearchCriteria searchCriteria)
+			throws DefaultExceptionHandler {
 		ResponseEntity<List<WeatherDetailsDTO>> response = null;
 		try {
 			List<WeatherDetailsDTO> weatherDetailsDTOList = weatherService.getAllWeatherDetails(searchCriteria);
@@ -46,8 +52,10 @@ public class WeatherController {
 		return response;
 	}
 
-	@DeleteMapping(name = "/weather-details/{city}")
-	public ResponseEntity<String> deleteWeatherDetails(@PathVariable String city) throws DefaultExceptionHandler {
+	@ApiOperation(value = "Delete record based on a particular city if present")
+	@DeleteMapping(value = "/weather-details/{city}", produces = "application/json")
+	public ResponseEntity<String> deleteWeatherDetails(@PathVariable(value = "city") String city)
+			throws DefaultExceptionHandler {
 		ResponseEntity<String> response = null;
 		try {
 			Optional<WeatherDetailsDTO> weatherDetailsDTO = weatherService.getWeatherDetailsByLocationName(city);
@@ -65,9 +73,10 @@ public class WeatherController {
 		return response;
 	}
 
+	@ApiOperation(value = "Create a new record in the database")
 	@PostMapping("/weather-details")
 	public ResponseEntity<WeatherDetailsDTO> addWeatherDetailsForSelectedCity(
-			@RequestBody WeatherDetailsDTO weatherDetailsDTO)
+			@ApiParam(value = "Pass weather details object to create a record in database") @RequestBody WeatherDetailsDTO weatherDetailsDTO)
 			throws LocationAlreadyPresentException, IncorrectInputDataException, DefaultExceptionHandler {
 		ResponseEntity<WeatherDetailsDTO> response = new ResponseEntity<>(HttpStatus.NO_CONTENT);
 		try {
--- weather-app/src/main/java/com/epam/weatherapp/dto/WeatherDetailsDTO.java
+++ weather-app/src/main/java/com/epam/weatherapp/dto/WeatherDetailsDTO.java
@@ -2,17 +2,24 @@ package com.epam.weatherapp.dto;
 
 import org.springframework.stereotype.Component;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 @Component
+@ApiModel(description = "Details of weather ")
 public class WeatherDetailsDTO {
 
+	@ApiModelProperty(notes = "City name")
 	private String locationName;
-
+	@ApiModelProperty(notes = "Temperature")
 	private String temperature;
+	@ApiModelProperty(notes = "Country name")
 	private String countryName;
+	@ApiModelProperty(notes = "State name")
 	private String stateName;
+	@ApiModelProperty(notes = "Humidity")
 	private String humidity;
 
 }
